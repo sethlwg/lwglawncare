86a987582126df51e7b1d61daeace86ff8ada104
Seth <sethlwg1@gmail.com>
2022-11-08 22:20:54 -0600

    This is a new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatToolbarModule} from '@angular/material/toolbar';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ServicesComponent } from './components/services/services.component';
+import { MatMenuModule }   from '@angular/material/menu';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: PageNotFoundComponent},
+  {path:  'services', component:ServicesComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    ContactComponent,
+    ServicesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
