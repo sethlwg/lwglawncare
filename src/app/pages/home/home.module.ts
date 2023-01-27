d0c93e036eb56a7fa72e836142c0ae4b19daf951
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-01-27 15:56:07 -0400

    add Typewriter effect

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import {NgxTypedJsModule} from 'ngx-typed-js';
+
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgxTypedJsModule],
 })
 export class HomeModule {}
