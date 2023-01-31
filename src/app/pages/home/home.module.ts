049b06768de8626f5b465c8843b9c6a1cfd3bfc3
Seth <sethlwg1@gmail.com>
2023-01-31 16:12:44 -0600

    upgrade ts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import {NgxTypedJsModule} from 'ngx-typed-js';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  imports: [CommonModule, NgxTypedJsModule,AppRoutingModule],
+  imports: [CommonModule, NgxTypedJsModule, AppRoutingModule],
 })
 export class HomeModule {}
