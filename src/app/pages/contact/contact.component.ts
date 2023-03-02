a87f109b30aa6309fcded3a8e05e0e10f6e530d1
Seth <sethlwg1@gmail.com>
2023-03-02 11:08:56 -0600

    ContactForm

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  firstName: any;
+  lastName: any;
+  email: any;
+  phone: any;
+  message: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/contact/contact.module.ts
+++ src/app/pages/contact/contact.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ContactComponent],
-  exports:[ContactComponent],
-  imports: [
-    CommonModule,
-  ]
+  exports: [ContactComponent],
+  imports: [CommonModule, FormsModule],
 })
-export class ContactModule { }
+export class ContactModule {
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  message!: string;
+  phone!: string;
+
+  submitForm() {}
+}
