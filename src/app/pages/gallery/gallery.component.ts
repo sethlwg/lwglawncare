86c356779b4049b3b373e7a02b0d41b38c2c91df
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-04-08 23:28:16 -0400

    build cards for images

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -7,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
   constructor() {}
-  lawns = [
+  images = [
     {
-      title: 'front',
-      image: 'assets/IMG_2236.JPG',
+      beforeImage: 'assets/IMG_3165.JPG',
+      afterImage: 'assets/IMG_3196.JPG',
+      caption: 'Flowerbed 1',
     },
+    {
+      beforeImage: 'assets/IMG_2871.JPG',
+      afterImage: 'assets/IMG_2885.JPG',
+      caption: 'Flowerbed 2',
+    },
+  ];
+  lawns = [
+    { lawn: 'assets/IMG_9188.JPG', caption: 'client 1' },
+    { lawn: 'assets/IMG_9181.JPG', caption: 'client 2' },
+    { lawn: 'assets/IMG_8947.JPG', caption: 'client 3' },
+    { lawn: 'assets/IMG_8949.JPG', caption: 'client 4' },
+    { lawn: 'assets/IMG_2236.JPG', caption: 'client 5' },
+    { lawn: 'assets/IMG_2685.JPG', caption: 'client 6' },
   ];
 
   ngOnInit(): void {}
--- src/app/pages/gallery/gallery.module.ts
+++ src/app/pages/gallery/gallery.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryComponent } from './gallery.component';
-
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [GalleryComponent],
-  exports:[GalleryComponent],
-  imports: [
-    CommonModule
-  ]
+  exports: [GalleryComponent],
+  imports: [CommonModule, MatGridListModule, MatCardModule],
 })
-
-export class GalleryModule { }
+export class GalleryModule {}
